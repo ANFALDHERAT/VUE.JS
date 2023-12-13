abb3d1e2397336246092fff5a2e43a20b7ce72b0
Anfal Dherat <anfaldherat7@gmail.com>
2023-12-13 13:00:24 +0300

    update

---
+++ slots/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div >
+<form-helper>
+  <!-- <h2 slot="title">{{title}}</h2>
+  <p slot="text">the paragraph tag for the slots</p> -->
+
+  <div slot="form-header">
+    <h3>"this is the title of form header"</h3>
+    <p>information about form</p>
+  </div>
+  <div slot="form-fields">
+<input type="text" placeholder="name" required>
+<input type="password" placeholder="password" required>
+  </div>
+  <div slot="form-controls">
+    <button v-on:click="handelSubmit">Submit</button>
+  </div>
+</form-helper>
+
+  </div>
+</template>
+
+<script>
+
+import formHelper from './components/formHelper.vue';
+
+export default {
+
+components:
+{
+'form-helper':formHelper
+
+},
+data(){
+  return {
+title:'I am adynamic title'
+  }
+}
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ slots/src/components/formHelper.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- <header>
+<slot name="title"></slot>
+<h1>I am a form helper</h1>
+<slot name="text"></slot>
+  </header> -->
+
+  <div>
+    <h1>please fill out our form</h1>
+    <form>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+      <div id="form-fields">
+        <slot name="form-fields"></slot>
+      </div>
+      <div id="form-controls">
+        <slot name="form-controls"></slot>
+      </div>
+      <div id="useful-link">
+        <ul>
+          <li><a href='#'>link1</a></li>
+          <li><a href='#'>link2</a></li>
+          <li><a href='#'>link3</a></li>
+          <li><a href='#'>link4</a></li>
+        </ul>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+
+
+data(){
+  return {
+
+  }
+},
+
+}
+</script>
+
+<style scoped>
+
+</style>
