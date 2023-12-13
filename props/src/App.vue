d1249352d1486450b237bba71eb57fbd463503af
Anfal Dherat <anfaldherat7@gmail.com>
2023-12-13 09:49:33 +0300

    update

---
+++ props/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div >
+
+<app-header>    </app-header>
+<app-anfal v-bind:anfals="anfals"></app-anfal>
+<app-footer>    </app-footer>
+  </div>
+</template>
+
+<script>
+
+// Update import statements in App.vue
+import Header from './components/Header.vue'
+
+import Footer from './components/footer.vue'
+import anfals from './components/anfals.vue'
+
+export default {
+
+components:
+{
+'app-header' :Header,
+'app-footer':Footer,
+'app-anfal':anfals,
+},
+data(){
+  return {
+   anfals:[
+    {name:'anfal',age:'20',show:false},
+    {name:'an',age:'20',show:false},
+    {name:'a',age:'20',show:false},
+    {name:'anfa',age:'20',show:false},
+   ]
+  }
+}
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ props/src/components/anfals.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="anfal">
+  <!-- <p>{{ anfals }}</p> -->
+  <ul>
+    <li v-for="anfal in anfals" v-on:click="anfal.show= !anfal.show">   <h1>{{ anfal.name }}</h1> </li>
+  </ul>
+</div>
+</template>
+
+<script>
+
+
+
+
+export default {
+props:{
+anfals:{
+  type:Array,
+  required:true
+}
+},
+  data(){
+  return {
+
+  }
+},
+// methods:{
+//   test:function(){
+//     this.anfals
+//   }
+
+
+// }
+
+}
+</script>
+
+<style scoped>
+
+</style>
