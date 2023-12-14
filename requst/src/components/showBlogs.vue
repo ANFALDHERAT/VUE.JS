880591c35ff5b79234fb50b4c1311b318325bb57
Anfal Dherat <anfaldherat7@gmail.com>
2023-12-14 11:15:43 +0300

    update

---
+++ requst/src/components/showBlogs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div v-theme:column="'wide'" id="show-blogs">
+<h1>All blog artical</h1>
+<div  v-for="blog in blogs"  class="single-blog">
+  <h1 v-rainbow>{{blog.title}}</h1>
+  <p>{{blog.body}}</p>
+</div>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+
+data(){
+  return {
+blogs:[
+
+]
+
+  }
+},
+methods:{
+
+},
+created()
+{
+  this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data)
+  {
+// console.log(data);
+this.blogs=data.body.slice(0,10);
+  })
+}
+
+}
+</script>
+
+<style scoped>
+#show-blogs{
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog
+{
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
---
+++ requst/src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueResource from 'vue-resource';
+Vue.config.productionTip = false
+Vue.use(VueResource);
+/* eslint-disable no-new */
+
+
+
+//custom directives
+Vue.directive('rainbow',{
+  bind(el,binding,vnode){
+el.style.color="red";
+  }
+})
+
+Vue.directive('theme',{
+  bind(el,binding,vnode){
+    if(binding.value == 'wide')
+    {
+      el.style.maxWidth='1200px';
+
+    }
+  }
+})
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
