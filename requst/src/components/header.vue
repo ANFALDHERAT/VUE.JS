99276c0b847ec51920f46656336fc9bd344f19b4
Anfal Dherat <anfaldherat7@gmail.com>
2023-12-14 13:49:40 +0300

    update

---
+++ requst/src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+<nav>
+  <ul>
+    <li><router-link to="/" exact>Blog</router-link></li>
+    <li><router-link to="/show" exact>list blog</router-link></li>
+  </ul>
+</nav>
+</template>
+
+<script>
+
+export default {
+
+
+data(){
+  return {
+
+
+  }
+}
+
+
+}
+</script>
+
+<style scoped>
+.router-link-active{
+  background-color: blueviolet;
+}
+</style>
